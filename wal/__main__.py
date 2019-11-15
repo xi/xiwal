e255b8439a45f2e9a7f152c639c9cbc8b1a97da1
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-11-15 09:05:30 +0100

    add argparse

--- wal/__main__.py
+++ wal/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 from . import image
 from . import scheme
@@ -10,10 +11,27 @@ gruvbox = ['#3c3836', '#cc241d', '#98971a', '#d79921', '#458588', '#b16286', '#6
 tango = ['#555753', '#ef2929', '#8ae234', '#fce94f', '#739fcf', '#ad7fa8', '#34e2e2']
 
 
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--image', '-i')
+	parser.add_argument('-n', type=int, default=8)
+	parser.add_argument('colors', nargs='*')
+	return parser.parse_args()
+
+
 if __name__ == '__main__':
-	colors = list(image.extract_colors(sys.argv[1], 9))
-	# colors = solarized
+	args = parse_args()
+
+	colors = []
+	if args.image:
+		colors += list(image.extract_colors(args.image, args.n))
+	colors += args.colors
+
+	if len(colors) < 6:
+		sys.exit('Need at least 6 colors')
+
 	scheme = scheme.colors2scheme(colors)
+
 	print(';'.join(scheme))
 	term.palette()
 	term.apply(scheme)
