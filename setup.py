12a26c460c7e421fe0a93fc2dbf0e4de8a95ac3e
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-11-17 09:06:29 +0100

    rename to xiwal

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 
 setup(
-    name='wal',
+    name='xiwal',
     version='0.0.0',
     description='Generate terminal color schemes',
     author='Tobias Bengfort',
     author_email='tobias.bengfort@posteo.de',
     packages=find_packages(),
     entry_points={'console_scripts': [
-        'wal=wal.__main__:main',
+        'xiwal=xiwal.__main__:main',
     ]},
     license='MIT',
 )
