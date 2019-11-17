ea787877efa010109fff40e9504e7989eb0d377a
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-11-17 11:24:24 +0100

    add github url to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='xiwal',
     version='0.0.0',
     description='Generate terminal color schemes',
+    url='https://github.com/xi/xiwal',
     author='Tobias Bengfort',
     author_email='tobias.bengfort@posteo.de',
     packages=find_packages(),
